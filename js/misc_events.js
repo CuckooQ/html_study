79bbcab95b173430fd43801f324270f5b8e1d2f8
jaeyong4536 <jaeyong4536@gmail.com>
2021-01-11 15:57:33 +0900

    misc events

---
+++ js/misc_events.js
@@ -0,0 +1,11 @@
+const details = document.querySelector("#details");
+
+const onToggle = () => {
+  console.log("toggle");
+}
+
+const init = () => {
+  details.addEventListener("toggle", onToggle);
+}
+
+init();
